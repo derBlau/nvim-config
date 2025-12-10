8021ac546ba76dfe63c7a30a9f8b7a19ddbaf65f
derBlau <176357385+derBlau@users.noreply.github.com>
2025-12-10 16:16:19 -0300

    Adds new <C-d> Telescope keymap
    The keymap makes it able to pull nvim config folder from anywhere

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,6 +8,12 @@ return {
     local set = vim.keymap.set
     local builtin = require("telescope.builtin")
 
+    -- goes into nvim config folder from anywhere
+    local pull_dotfiles = function()
+      builtin.find_files( { cwd = vim.fn.stdpath("config") })
+    end
+
+    set("n", "<C-d>", pull_dotfiles, { desc = "Find nvim config dir" })
     set("n", "<C-f>", builtin.find_files, { desc = "Find files in cwd" })
     set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
 
